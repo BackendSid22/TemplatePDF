fb25987377228479f0f9b634a64245d1a11c3280
BackendSid22 <168301526+BackendSid22@users.noreply.github.com>
2024-07-27 22:28:39 +0300

    Added more pages for each topic by iterating using ranges

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ for index, row in df.iterrows():
     pdf.set_font(family="Times", style="B", size=24)
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
-    pdf.line(x1=10,y1=22, x2=200,y2=22)
+    pdf.line(x1=10, y1=22, x2=200, y2=22)
+
+    for i in range(row["Pages"] - 1):
+        pdf.add_page()
 
 pdf.output("output.pdf")
